721ccc46a6a3fa744c91f7434acfa9501b995213
Muhammad Ansar <57707946+embeddedlab786@users.noreply.github.com>
2020-05-17 01:50:00 +0500

    Update Automatic_Light_Control.ino

--- Automatic_Light_Control.ino
+++ Automatic_Light_Control.ino
@@ -1,7 +1,7 @@
 #include <LiquidCrystal.h>// initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
 
-#define LDR_Sensor  A0  //10k Variable Resistor
+#define LDR_Sensor  A0  //LDR Sensor Pin
 
 #define relay 8 // Out for light 
 
@@ -24,8 +24,8 @@ lcd.clear();
 }
 
 void loop(){   
-read_ADC = analogRead(LDR_Sensor);
-Brightness = read_ADC /10.24;
+read_ADC = analogRead(LDR_Sensor); // read analogue to digital value 0 to 1023
+Brightness = read_ADC /10.23; // Maximum value analogue to digital 1023 / 10.23
 
 lcd.setCursor(0, 0);
 lcd.print("Brightness: ");
